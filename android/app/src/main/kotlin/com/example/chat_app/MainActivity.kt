8a1bd9931013759e65e81fe3872623cc7361fb89
kaytech <kayodeoloyede09911@gmail.com>
2025-07-25 16:39:24 +0100

    done setting up firebase store

---
+++ android/app/src/main/kotlin/com/example/chat_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.chat_app.app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:chat_app/screen/chat.dart';
+import 'package:chat_app/screen/splash.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:chat_app/screen/auth.dart';
+
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'FlutterChat',
+      theme: ThemeData().copyWith(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue.shade900),
+      ),
+      home: StreamBuilder(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (ctx, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const SplashScreen();
+          }
+          if (snapshot.hasData) {
+            return ChatScreen();
+          }
+          return const AuthScreen();
+        },
+      ),
+    );
+  }
+}
